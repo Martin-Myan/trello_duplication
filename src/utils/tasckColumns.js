cfc475324a3b4420ad7860f0791dd8b9aa7f5090
Martirosyan Mrd <martirosyanmrd@gmail.com>
2021-09-23 19:10:15 +0400

    Added add piller functionality, Added removal piller functionality, Added removal item functionality

--- src/utils/tasckColumns.js
+++ src/utils/tasckColumns.js
@@ -3,18 +3,6 @@ const tasckColumns = [
     id: 1,
     title: "1 Section",
   },
-  // {
-  //   id: 2,
-  //   title: "2 Section",
-  // },
-  // {
-  //   id: 3,
-  //   title: "3 Section",
-  // },
-  // {
-  //   id: 4,
-  //   title: "4 Section",
-  // },
 ];
 
 export default tasckColumns;
--- src/utils/tasckItem.js
+++ src/utils/tasckItem.js
@@ -26,46 +26,6 @@ const taskcItem = [
     columnId: 1,
     description: "OOOOOOO",
   },
-  // {
-  //   id: shortid.generate(),
-  //   columnId: 2,
-  //   description: "789",
-  // },
-  // {
-  //   id: shortid.generate(),
-  //   columnId: 3,
-  //   description: "<<<<<|||>>>>>>",
-  // },
-  // {
-  //   id: shortid.generate(),
-  //   columnId: 4,
-  //   description: "!@#$%^&*",
-  // },
-  // {
-  //   id: shortid.generate(),
-  //   columnId: 4,
-  //   description: ")(*&^%",
-  // },
-  // {
-  //   id: shortid.generate(),
-  //   columnId: 3,
-  //   description: ">>>>>>|||<<<<<<",
-  // },
-  // {
-  //   id: shortid.generate(),
-  //   columnId: 4,
-  //   description: "&^%~",
-  // },
-  // {
-  //   id: shortid.generate(),
-  //   columnId: 4,
-  //   description: "++++++++++",
-  // },
-  // {
-  //   id: shortid.generate(),
-  //   columnId: 14,
-  //   description: "ahfeakhgfhadv",
-  // },
 ];
 
 export default taskcItem;
